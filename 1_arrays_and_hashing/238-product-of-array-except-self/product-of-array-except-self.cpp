d4648a0a680b43698456ff893da9265795f8b1c2
Ishita Prakash <136169172+ishita-prakash@users.noreply.github.com>
2024-02-22 22:42:23 +0530

    Time: 12 ms (88.31%) | Memory: 26.2 MB (55.17%) - LeetSync

---
+++ 1_arrays_and_hashing/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n, 1); // n elements in answer vector, ach initialised with 1
+
+        //edge cases
+
+
+        int leftProduct = 1;
+
+        for(int i=0; i<n; i++){
+            answer[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        int rightProduct = 1;
+
+        for(int i=n-1; i>=0; i--){
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+};
+
+/* //brute force - O(n^2)
+        int n = nums.size();
+        vector<int> answer;
+
+        //edge case
+        if(n == 0 || n == 1)
+            return answer;
+
+        for(int i=0; i<n; i++){
+            int temp = 1;
+            for(int j=0; j<n; j++){
+                if(i != j){
+                    temp = temp * nums[j];
+                }
+            }
+            answer.push_back(temp);
+        }
+
+        return answer; */
