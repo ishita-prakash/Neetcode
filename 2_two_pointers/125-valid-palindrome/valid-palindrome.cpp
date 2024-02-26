80cd25ba8b45cdc6edd5833652cfffa2a833a268
Ishita Prakash <136169172+ishita-prakash@users.noreply.github.com>
2024-02-26 22:42:26 +0530

    Time: 11 ms (10.05%) | Memory: 10.2 MB (7.86%) - LeetSync

---
+++ 2_two_pointers/125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        //edge case
+        if(s.length() == 0){
+            return true;
+        }
+
+        string s1; //store preprocessed string in s1
+
+        for(char x: s){
+            if(isalnum(x)){
+                if(x >= 'A' && x <= 'Z'){
+                    x += 32;
+                    s1 += x;
+                }
+                else{
+                    s1 += x;
+                }
+            }
+        }
+
+        stack<char> st;
+
+        for(char x: s1){
+            st.push(x);
+        }
+
+        string s2;
+
+        while(!st.empty()){
+            s2 += st.top();
+            st.pop();
+        }
+
+        if(s1 == s2)
+            return true;
+        else
+            return false;
+
+    }
+};
