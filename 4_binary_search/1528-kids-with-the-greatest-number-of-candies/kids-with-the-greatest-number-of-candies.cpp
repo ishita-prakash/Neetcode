2cbd990679500a649c27b8340e81efb16bd38d6c
Ishita Prakash <136169172+ishita-prakash@users.noreply.github.com>
2024-07-10 19:45:32 +0530

    Time: 3 ms (49.55%) | Memory: 10.8 MB (73.22%) - LeetSync

--- 4_binary_search/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 4_binary_search/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        vector<int> tmp = candies;
-        sort(tmp.begin(), tmp.end());
-        int max_val = tmp[candies.size() - 1];
+        int max_val = *max_element(candies.begin(), candies.end());
 
         vector<bool> res(candies.size(), false);
 
